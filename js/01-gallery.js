f3d153150e32faf0b15ded6d403ce5753494042f
Vladislav-UZH <perebziak.vladislav@gmail.com>
2022-10-30 18:47:53 +0300

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,59 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-const instance = basicLightbox.create(``)
 
-const galleryUlEl = document.querySelector('.gallery')
-
-galleryUlEl.addEventListener('click', (event) => {
-    event.preventDefault()
-    
-    if (event.target.nodeName !== 'IMG') {
-        return
-    }
-    basicLightbox.create(`
-	   <img src="${event.target.dataset.source}"> 
-`).show(onCheckModalWindowStatus())
- 
-});
-console.log(galleryItems);
-
-function markup() {
-    return galleryItems.map(({ preview, original, description }) => `
-    <li class="gallery__item">
-    <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    /></a>
-  </li>`).join('');
-}
-
-function onGetImages() {
-    
-    return galleryUlEl.insertAdjacentHTML('beforeend',markup())
-}
-onGetImages();
-function onCheckModalWindowStatus() {
-    if (!instance.visible()) {
-
-        console.log('It is open');
-        // basicLightbox.close()
-        
-    }
-    document.addEventListener('keyup', onEscKeyBtn);
-}
-const onEscKeyBtn = (ev) => {
-    if (ev.code === 'Escape') {
-        console.log('noup');
-        instance.close()
-            return
-        }
-        return instance.close()
-}
 //1 створити прослуховувач при кліку на картинку +
 //2 створити функцію яка буде створювати розмітку +
 //3 створити функцію, яка буде пушити в ДОМ розмітку +
 //4  створити функцію, яка буде перевіряти чи відкрита та закрита модалка 
 // і буде вішати прослуховувач на Esc , та знімати його.
+
+
+let instance = '';
+const gallery = document.querySelector('.gallery')
+const modalClickList = gallery.addEventListener('click',onModalWindowOpened)
+
+function markup() {
+   return galleryItems.map(({ preview, original, description }) => `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`).join('')
+}
+function onGetStartGalleryImg() {
+    gallery.insertAdjacentHTML('beforeend',markup())
+}
+onGetStartGalleryImg()
+
+function onModalWindowOpened(ev) {
+    ev.preventDefault()
+    if (ev.target.nodeName !== 'IMG') {
+        return;
+    };
+    instance = basicLightbox.create(`<img src="${ev.target.dataset.source}">`);
+    instance.show();
+    document.addEventListener('keyup',onCloseToEsc)
+}
+function onCloseToEsc(ev) {
+    if (ev.code !== 'Escape') {
+        return;
+    }
+    console.log('tap');
+    instance.close(()=> document.removeEventListener('keyup',onCloseToEsc))
+
+}
